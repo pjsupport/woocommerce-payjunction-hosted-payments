d73df7c0143593e200a5814da087594f11d9bf8a
pjsupport <support@payjunction.com>
2017-11-27 18:22:59 +0000

    fixed bug causing testmode to always resolve to true

--- wp-content/plugins/wc-payjunction-hosted-payments/wc-payjunction-hosted-payments-gateway.php
+++ wp-content/plugins/wc-payjunction-hosted-payments/wc-payjunction-hosted-payments-gateway.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: PayJunction Hosted Payments Gateway Module for WooCommerce
 Description: Credit Card Processing Module for WooCommerce using the PayJunction Hosted Payments service
-Version: 1.0.0
+Version: 1.0.1
 Plugin URI: https://company.payjunction.com/support/WooCommerce
 Author: Matthew E. Cooper
 Author URI: https://www.payjunction.com
@@ -21,7 +21,7 @@ add_action( 'plugins_loaded', 'payjunction_hp_init', 0 );
 function payjunction_hp_init() {
     class WC_PayJunction_HP extends WC_Payment_Gateway {
         
-        const VERSION = "1.0.0";
+        const VERSION = "1.0.1";
         
         const PJ_TESTMODE_META_KEY = '_pj_hp_test_mode';
         const PJ_TESTMODE_TRUE = 'labs';
@@ -42,7 +42,7 @@ function payjunction_hp_init() {
             $this->sb_shopname          = $this->settings[ 'sb_shopname'    ];
             $this->title                = $this->settings[ 'title'          ];
             $this->customerror          = $this->settings[ 'customerror'    ];
-            $this->testmode             = $this->settings[ 'testmode'       ];
+            $this->testmode             = $this->settings[ 'testmode'       ] === 'yes' ? true :false;
             $this->description          = $this->settings[ 'description'    ];
             $this->pay_desc             = $this->settings[ 'pay_desc'       ];
             $this->debugging            = $this->settings[ 'debugging'      ] === 'yes' ? true : false;
